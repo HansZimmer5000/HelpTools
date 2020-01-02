b68da8a30069e112a1b0f155f18805d7eba157e6
HansZimmer5000 <michael.weitnau@live.de>
2020-01-02 22:11:32 +0100

    add powertop and tlp

---
+++ powertops/energyconsumption.sh
@@ -0,0 +1,13 @@
+# Date in format of DDMMYYYY (D = Day, M = Month, Y = Year)
+PWD_DIR=$pwd
+FILE_NAME="powertop_$(date +'%d_%m_%Y')"
+DIR="."
+
+echo "Creating new Powertop file with name: $FILE_NAME"
+echo "Creating File in: $DIR"
+echo # Empty Line
+
+cd $DIR
+sudo powertop --csv="$FILE_NAME.csv"
+sudo powertop --html="$FILE_NAME.html" 
+cd $PWD_DIR
---
+++ tlp-tool/tlp-tool.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+print_help(){
+	echo "This tool sets a specific tlp config and then restart the tlp.service"
+	echo "Only one input is needed, according to the given number a specific config will be set:"
+	echo "0. template_config" 
+	echo "1. power_config" 
+	echo "2. performance_config" 
+	echo 
+	echo Current Config: $(cat ./current_config)
+}
+
+new_config=""
+
+if [ "$#" -eq 0 ]; then
+	print_help
+elif [ "$1" -eq 0 ]; then
+	new_config="template_config"
+elif [ "$1" -eq 1 ]; then
+	new_config="power_config"
+elif [ "$1" -eq 2 ]; then
+	new_config="performance_config"
+else 
+	print_help
+fi
+
+if [ "$new_config" != "" ]; then 
+	echo "Beware! This will replace the /etc/default/tlp file"
+	read -p "Setting $new_config, continue?"	
+	sudo cp -f ./$new_config /etc/default/tlp
+
+	echo "Restarting tlp.service"
+	sudo systemctl restart tlp.service
+
+	echo $new_config > ./current_config
+	sudo systemctl status tlp.service
+fi
