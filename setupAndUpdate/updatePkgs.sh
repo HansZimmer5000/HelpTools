5a436d858e3c38db9c8c0f211e33a74a1edf4358
Hape <hape@localhost.localdomain>
2020-08-24 20:40:51 +0200

    add distro check

--- setupAndUpdate/updatePkgs.sh
+++ setupAndUpdate/updatePkgs.sh
@@ -1,43 +1,58 @@
 #!/bin/sh
 set -e #Exit on error
 
-echo "--"
-echo "--"
-echo "Updating Pacman!"
-echo "--"
-echo "--"
-sudo pacman -Syyu
+if [[ "$(lsb_release -a )" == *"Manjaro"* ]]; then
+    echo "--"
+    echo "--"
+    echo "Updating Pacman!"
+    echo "--"
+    echo "--"
+    sudo pacman -Syyu
 
-echo "--"
-echo "--"
-echo "Updating PacAur!"
-echo "--"
-echo "--"
-pacaur -Syyu
+    echo "--"
+    echo "--"
+    echo "Updating PacAur!"
+    echo "--"
+    echo "--"
+    pacaur -Syyu
 
-echo "--"
-echo "--"
-echo "Cleaning up Pacman Cache!"
-echo "--"
-echo "--"
-sudo paccache -r
+    echo "--"
+    echo "--"
+    echo "Cleaning up Pacman Cache!"
+    echo "--"
+    echo "--"
+    sudo paccache -r
 
-echo "--"
-echo "--"
-echo "Listing orphan package!"
-echo "--"
-echo "--"
-# -q for package names without version
-sudo pacman -Qtd
+    echo "--"
+    echo "--"
+    echo "Listing orphan package!"
+    echo "--"
+    echo "--"
+    # -q for package names without version
+    sudo pacman -Qtd
 
-echo "--"
-echo "--"
-echo "Updating Flatpak!"
-echo "--"
-echo "--"
-flatpak update
+    echo "--"
+    echo "--"
+    echo "Updating Flatpak!"
+    echo "--"
+    echo "--"
+    flatpak update
 
 
-sudo pacman-mirrors -c Netherlands # or Germany, Greece, ...
+    sudo pacman-mirrors -c Netherlands # or Germany, Greece, ...
+elif [[ "$(lsb_release -a )" == *"Fedora"* ]]; then
+    echo "--"
+    echo "--"
+    echo "Updating DNF!"
+    sudo dnf update 
+    echo "--"
+    echo "--"
 
+    echo "--"
+    echo "--"
+    echo "Cleaning up DNF!"
+    sudo dnf autoremove -y
+    echo "--"
+    echo "--"
+fi
 
