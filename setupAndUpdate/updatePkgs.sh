6767c810eb89c85b1342ff7e496cb588be22bd85
Hape <hape@localhost.localdomain>
2020-08-24 21:13:56 +0200

    refactor

--- setupAndUpdate/updatePkgs.sh
+++ setupAndUpdate/updatePkgs.sh
@@ -1,58 +1,64 @@
 #!/bin/sh
 set -e #Exit on error
 
-if [[ "$(lsb_release -a )" == *"Manjaro"* ]]; then
+update_pacman(){
     echo "--"
     echo "--"
     echo "Updating Pacman!"
-    echo "--"
-    echo "--"
     sudo pacman -Syyu
+}
 
+update_pacaur(){
     echo "--"
     echo "--"
     echo "Updating PacAur!"
-    echo "--"
-    echo "--"
     pacaur -Syyu
+}
 
-    echo "--"
-    echo "--"
-    echo "Cleaning up Pacman Cache!"
-    echo "--"
-    echo "--"
-    sudo paccache -r
-
-    echo "--"
-    echo "--"
-    echo "Listing orphan package!"
-    echo "--"
-    echo "--"
-    # -q for package names without version
-    sudo pacman -Qtd
-
-    echo "--"
-    echo "--"
-    echo "Updating Flatpak!"
-    echo "--"
-    echo "--"
-    flatpak update
-
-
-    sudo pacman-mirrors -c Netherlands # or Germany, Greece, ...
-elif [[ "$(lsb_release -a )" == *"Fedora"* ]]; then
+update_dnf(){
     echo "--"
     echo "--"
     echo "Updating DNF!"
     sudo dnf update 
-    echo "--"
-    echo "--"
+}
+
+clean_dnf(){
 
     echo "--"
     echo "--"
     echo "Cleaning up DNF!"
     sudo dnf autoremove -y
+}
+
+clean_pacman(){
     echo "--"
     echo "--"
+    echo "Cleaning up Pacman Cache!"
+    sudo paccache -r
+}
+
+update_flatpak(){
+    echo "--"
+    echo "--"
+    echo "Updating Flatpak!"
+    flatpak update
+}
+
+if [[ "$(lsb_release -a )" == *"Manjaro"* ]]; then
+    update_pacman
+    update_pacaur
+    update_flatpak
+
+    clean_pacman
+    echo "--"
+    echo "--"
+    echo "Listing orphan package!"
+    # -q for package names without version
+    sudo pacman -Qtd
+    sudo pacman-mirrors -c Netherlands # or Germany, Greece, ...
+elif [[ "$(lsb_release -a )" == *"Fedora"* ]]; then
+    update_dnf
+    update_flatpak
+    clean_dnf
 fi
 
