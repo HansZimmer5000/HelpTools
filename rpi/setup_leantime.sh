ace75adf9ce1e320d63d1cc6b6d48b52fbb1dd42
Michael Müller <michael.mueller@cloudflight.io>
2021-01-02 20:04:46 +0100

    refactor and add backup timer

--- rpi/setup_leantime.sh
+++ rpi/setup_leantime.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 
+set_service_file(){
+    echo "$2" > tmp
+    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S rm /etc/systemd/system/$1" 1>/dev/null 2>&1
+    scp tmp "$non_sudo_user"@"$server_url":/home/hape/$1 1>/dev/null 2>&1
+    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S mv /home/hape/$1 /etc/systemd/system/$1" 1>/dev/null 2>&1
+}
+
+set_service_systemd(){
+    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S systemctl daemon-reload" 1>/dev/null 2>&1
+    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S systemctl enable $1" 1>/dev/null 2>&1
+    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S systemctl stop $1" 1>/dev/null 2>&1
+    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S systemctl start $1" 1>/dev/null 2>&1
+}
+
+set_service_cleanup(){
+    rm tmp
+}
+
+# $1 = service name
+# $2 = service code
+set_service(){
+    if [ -z "$1" ]; then
+        echo "Param 1 not given!"
+    elif [ -z "$2" ]; then
+        echo "Param 2 not given!"
+    else
+        echo "Setting Service: $1"
+        set_service_file "$1" "$2"
+        set_service_systemd "$1"
+        set_service_cleanup
+    fi
+}
+
 set_dump_sql(){
     save_files=($(ls ../../mathesis/tickets/saves/))
     dump_file_index=$((${#save_files[@]}-1))
     dump_file=${save_files[dump_file_index]}
 
     read -p "This will delete the current db and import from dump.sql, is that what you want?"
-    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S rm -r /home/hape/dump.sql /home/hape/db_data/*"
+    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S rm -fr /home/hape/dump.sql /home/hape/db_data/*"
     scp ../../mathesis/tickets/saves/$dump_file "$non_sudo_user"@"$server_url":/home/hape/dump.sql 1>/dev/null 2>&1
 }
 
 set_leantime_service(){
     service_name=leantime.service
-    
-    # podman run -it -p 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=leantime  -v $PWD/db21-01-01_19-34-03.sql:/docker-entrypoint-initdb.d/dump.sql -e MYSQL_USER=admin -e MYSQL_PASSWORD=pass mysql:5.7
-
-    # Create Service file
     service="[Unit]
 Description=Start Leantime
 After=networking.service
@@ -32,39 +61,45 @@ ExecStart=/usr/bin/bash -c '\
     /usr/bin/docker run -d --net host -p 0.0.0.0:80:80 --name leantime -e LEAN_DB_DATABASE=leantime -e LEAN_DB_USER=admin -e LEAN_DB_PASSWORD=pass -e LEAN_DB_HOST=127.0.0.1 leantime:latest ;\
     exit 0'
 
+[Install]
+WantedBy=multi-user.target
+"
+    set_service $service_name "$service"
+}
+
+set_backup_service(){  
+    service_name=leantime-backup.service
+    service="[Unit]
+Description=Backup Leantime
+After=networking.service
+After=leantime.service
+
+[Service]
+ExecStart=/usr/bin/docker exec -u root mysql bash -c 'mysqldump -u root --password=pass leantime' > /home/hape/backup.sql
 
 [Install]
 WantedBy=multi-user.target
 "
-    echo "$service" > tmp
-    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S rm /etc/systemd/system/$service_name /etc/systemd/system/multi-user.target.wants/$service_name /home/hape/docker-compose.yml" #1>/dev/null 2>&1 
-    scp tmp "$non_sudo_user"@"$server_url":/home/hape/$service_name 1>/dev/null 2>&1
-    scp leantime-compose.yaml "$non_sudo_user"@"$server_url":/home/hape/docker-compose.yml 1>/dev/null 2>&1
+    set_service $service_name "$service"
 
-    # Move Service to server
-    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S mv /home/hape/$service_name /etc/systemd/system/$service_name" #1>/dev/null 2>&1 
+    service_name=leantime-backup.timer
+    service="[Unit]
+Description=Backup Leantime every 10 minutes
+After=networking.service
+After=leantime.service
 
-    # Update systemd
-    echo reload
-    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S systemctl daemon-reload" #1>/dev/null 2>&1 
-    echo stop
-    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S systemctl stop $service_name" #1>/dev/null 2>&1 
-    echo "enable"
-    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S systemctl enable $service_name" #1>/dev/null 2>&1 
-    echo start
-    ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S systemctl start $service_name" #1>/dev/null 2>&1 
+[Timer]
+OnCalendar=*:0/10
 
-    # Clean up
-    rm tmp
-}
-
-set_backup_service(){
-    : #Setup Systemd servcie that dumps the leantime database every hour (up to three parallel saves, if the fourth would come the oldest of the three will be overwritten)
+[Install]
+WantedBy=timers.target
+"
+    set_service $service_name "$service"
 }
 
 non_sudo_user=hape
 server_url=raspberrypinew
-read -rsp "Input Sudo Pw: " SUDO_PW
+read -rsp "Input Server User Password: " SUDO_PW
 echo
 
 if [ "$1" = "dump" ]; then
