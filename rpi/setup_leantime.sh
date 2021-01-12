4859d5d913d14f51db45f6deed10cdece7aebcbd
Michael Müller <michael.mueller@cloudflight.io>
2021-01-12 08:11:58 +0100

    add services in own files

--- rpi/setup_leantime.sh
+++ rpi/setup_leantime.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# sysbench --test=cpu --cpu-max-prime=10000 --num-threads=4 run
+# vcgencmd measure_temp 
+# cat /proc/loadavg
+# watch $(( $(cat /sys/devices/virtual/thermal/thermal_zone0/temp) / 100))"°"
+
 set_service_file(){
     echo "$2" > tmp
     ssh "$non_sudo_user"@"$server_url" "echo -n $SUDO_PW | sudo -S rm /etc/systemd/system/$1" 1>/dev/null 2>&1
@@ -45,58 +50,17 @@ set_dump_sql(){
 
 set_leantime_service(){
     service_name=leantime.service
-    service="[Unit]
-Description=Start Leantime
-After=networking.service
-
-[Service]
-ExecStart=/usr/bin/bash -c '\
-    /usr/bin/docker rm -f leantime; \
-    /usr/bin/docker rm -f mysql; \
-    sleep 5s; \
-    dump=''; \
-    /usr/bin/docker run -d -p 127.0.0.1:3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=leantime -e MYSQL_USER=admin -e MYSQL_PASSWORD=pass -v /home/hape/db_data:/var/lib/mysql -v /home/hape/dump.sql:/docker-entrypoint-initdb.d/dump.sql biarms/mysql:latest --character-set-server=utf8 --collation-server=utf8_unicode_ci;\
-    sleep 45s; \
-    rm -f /home/hape/dump.sql; \
-    /usr/bin/docker run -d --net host -p 0.0.0.0:80:80 --name leantime -e LEAN_DB_DATABASE=leantime -e LEAN_DB_USER=admin -e LEAN_DB_PASSWORD=pass -e LEAN_DB_HOST=127.0.0.1 leantime:latest; \
-    exit 0'
-
-[Install]
-WantedBy=multi-user.target
-"
+    service="$(cat services/leantime.service)"
     set_systemd_unit $service_name "$service"
 }
 
 set_backup_service(){  
     service_name=leantime-backup.service
-    service="[Unit]
-Description=Backup Leantime
-After=networking.service
-After=leantime.service
-
-[Service]
-ExecStart=bash -c '\
-    rm -rf /home/hape/backup2.sql; \
-    test -f /home/hape/backup.sql && mv /home/hape/backup.sql /home/hape/backup2.sql; \
-    /usr/bin/docker exec -u root mysql mysqldump -u root --password=pass leantime 1> /home/hape/backup.sql'
-
-[Install]
-WantedBy=multi-user.target
-"
+    service="$(cat services/leantime-backup.service)"
     set_systemd_unit $service_name "$service"
 
     service_name=leantime-backup.timer
-    service="[Unit]
-Description=Backup Leantime every 10 minutes
-After=networking.service
-After=leantime.service
-
-[Timer]
-OnCalendar=*:0/10
-
-[Install]
-WantedBy=timers.target
-"
+    service="$(cat services/leantime-backup.timer)"
     set_systemd_unit $service_name "$service"
 }
 
