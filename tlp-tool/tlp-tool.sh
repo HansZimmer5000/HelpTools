58f308147270bcf03bc9ff61c0d9a8b2749b7e2c
Michael Müller <michael.mueller@cloudflight.io>
2021-09-26 14:25:27 +0200

    refactor tlp

--- tlp-tool/tlp-tool.sh
+++ tlp-tool/tlp-tool.sh
@@ -1,38 +1,42 @@
-#!/bin/sh
-set -e #Exit on error
+#!/bin/bash
+set -e 
 
 print_help(){
-	echo "This tool sets a specific tlp config and then restart the tlp.service"
-	echo "Only one input is needed, according to the given number a specific config will be set:"
-	echo "0. template_config" 
-	echo "1. power_config" 
-	echo "2. performance_config" 
-	echo 
-	echo Current Config: $(cat ./current_config)
+	echo "
+This tool sets a specific tlp config and then restart the tlp.service
+Only one input is needed after executing the script: 
+According to the given number a specific config will be set:
+0. template_config
+1. power_config
+2. performance_config
+
+Current Config: ${current_config}
+"
 }
 
+current_config=$(cat ./current_config || echo 'n/a')
 new_config=""
 
-if [ "$#" -eq 0 ]; then
-	print_help
-elif [ "$1" -eq 0 ]; then
-	new_config="template_config"
-elif [ "$1" -eq 1 ]; then
-	new_config="power_config"
-elif [ "$1" -eq 2 ]; then
-	new_config="performance_config"
-else 
-	print_help
-fi
+select input in template_config power_config performance_config; do	
+	case $input in
+		"")
+			print_help
+			;;
+		*_config)
+			new_config="$input"
+			;;
+	esac
+	break
+done
 
-if [ "$new_config" != "" ]; then 
+if [ -n "$new_config" ]; then 
 	echo "Beware! This will replace the /etc/default/tlp file"
-	read -p "Setting $new_config, continue?"	
-	sudo cp -f ./$new_config /etc/tlp.conf
+	read -rp "Setting $new_config, continue?"	
+	sudo cp -f ./"$new_config" /etc/tlp.conf
 
 	echo "Restarting tlp.service"
 	sudo systemctl restart tlp.service
 
-	echo $new_config > ./current_config
+	echo "$new_config" > ./current_config
 	sudo systemctl status tlp.service
 fi
