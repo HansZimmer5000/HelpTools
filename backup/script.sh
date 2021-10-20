7da138068729d8fd561bca8a9a4224bf68943bf8
Michael Müller <michael.mueller@cloudflight.io>
2021-10-20 11:05:52 +0200

    extend rsync

--- backup/script.sh
+++ backup/script.sh
@@ -2,7 +2,15 @@
 
 sub_folder="Aufräumen"
 
-src="/run/media/hape/Michis BackUP/$sub_folder/" # Trailing Slash important
-trg="/run/media/hape/Seagate/$sub_folder"
+disk1="/run/media/hape/Michis BackUP"
+disk2="/run/media/hape/MichisBackUp1"
+
+src="$disk1/$sub_folder/" # Trailing Slash important
+trg="$disk2/$sub_folder"
 
 rsync -av --dry-run --delete "$src" "$trg" &>rsync-dry.log
+
+read -p "Continue without dry-run? (y/n): " -n 1
+if [ "$REPLY" = "y" ]; then
+    rsync -av --delete "$src" "$trg" &>rsync.log
+fi
