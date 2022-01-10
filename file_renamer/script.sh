b089ba3afe5b7f0b222b216879bd574b6de6a8cb
HaPe <michael.mueller@ing.com>
2022-01-10 15:06:55 +0100

    extend renamer

--- file_renamer/script.sh
+++ file_renamer/script.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-prepare_environment(){
-    if ! [ -f "$folder/names.csv" ]; then
-        echo "Cannot find names.csv in folder: $folder"
+check_filename_v2(){
+    filename="$1"
+    file_age="$2"
+
+    if [[ "$filename" == *"O."*"Nr."* ]]; then 
+        echo "Skipping $file_age FileName $filename due to name 'O. Nr.'"
+    elif [[ "$filename" == *"TODO"* ]]; then 
+        echo "Skipping $file_age FileName $filename due to place 'TODO'"
+    elif [[ "$filename" == *"?"* ]]; then 
+        echo "Skipping $file_age FileName $filename due to '?' in Inv.Nr. or Description"
+    elif [[ "$filename" == *"/"* ]]; then 
+        echo "Skipping $file_age FileName $filename due to '/' in Inv.Nr. or Description"
+    fi
+}
+
+prepare_environment_v1(){
+    if ! [ -f "$names_csv" ]; then
+        echo "Cannot find $names_csv"
         exit 1
     fi
 
-    files_to_rename=($(ls $folder | sed 's|names.csv||g'))
+    # TODO weird code line to have if names_csv is somewhere else (and so the names_csv has a path included)
+    files_to_rename=($(ls $folder | sed "s|$names_csv||g"))
     new_file_names=
 
     while IFS="$csv_delimiter" read -r name rest; do
         new_file_names=(${new_file_names[@]} "$name")
-    done < "$folder/names.csv"
+    done < "$names_csv"
 
     file_count="${#files_to_rename[@]}"
     name_count="${#new_file_names[@]}"
     if [ "$file_count" != "$name_count" ]; then
         echo "Length is not equal: file count ($file_count), name count ($name_count)"
+        echo "$folder $PWD"
         exit 1
     fi
 }
 
-rename_files(){
+rename_files_v1(){
     for ((i=0; i<"$name_count"; i++)); do
         file=${files_to_rename[$i]}
         name=${new_file_names[$i]}
@@ -31,13 +48,100 @@ rename_files(){
     done
 }
 
-parse_input(){
-    test -n "$1" && folder="$1"
+rename_files_v2(){
+    unset new_file_names
+
+    row_count=1
+    while read -r ; do
+        if [ "$row_count" -eq 1 ]; then
+            row_count=$((row_count+1))
+            continue
+        fi
+    
+        filenames=$(echo "$REPLY" | cut -d"$csv_delimiter" -f1)
+        invno=$(echo "$REPLY" | cut -d"$csv_delimiter" -f3)
+        description=$(echo "$REPLY" | cut -d"$csv_delimiter" -f4)
+        place=$(echo "$REPLY" | cut -d"$csv_delimiter" -f6)
+
+        if [ -z "$filenames" ]; then
+            echo "Skipping, no filename(s)"
+        fi
+
+        unset new_file_name_basis
+        if [ -n "$invno" ]; then
+            new_file_name_basis="$invno"
+        elif [ -n "$description" ]; then
+            new_file_name_basis="$description" # TODO maybe replace " " with "_"
+        else
+            echo "Skipping: $filenames do not have Inv.Nr. or Description"
+            row_count=$((row_count+1))
+            continue
+        fi
+
+        filename_no=1
+        file_suffix="JPG"
+        for filename in $filenames; do
+            test "$filename" = "&" && continue
+            filename_check_result=$(check_filename_v2 "$filename" "Old")
+            if [ -n "$filename_check_result" ]; then
+                echo "$filename_check_result"
+                continue
+            fi
+            
+            new_file_name="$new_file_name_basis-$filename_no.$file_suffix"
+            filename_check_result=$(check_filename_v2 "$new_file_name" "New")
+            if [ -n "$filename_check_result" ]; then
+                echo "$filename_check_result"
+                continue
+            fi
+
+            mkdir -p "$folder/$place"
+            echo "Moving $folder/$filename.$file_suffix to $folder/$place/$new_file_name"
+            mv "$folder/$filename.$file_suffix" "$folder/$place/$new_file_name"
+            filename_no=$((filename_no+1))
+        done
+        row_count=$((row_count+1))
+
+        # TODO
+        # Iterate through file name
+        # rename each to "<name (Inv. No or Description)>-<no>.<old file suffix>"
+    done < "$names_csv"
 }
 
-csv_delimiter=","
-folder="$PWD"
+parse_input(){
+    test -n "$1" && folder="$1"
+    folder=${folder%/}
+
+    version=1
+    if [ "$1" = "v2" ] || [ "$2" = "v2" ]; then
+        version=2
+    fi
+
+    echo "Variables:
+    algorithm_version=$version
+    csv_delimiter=$csv_delimiter
+    folder=$folder
+    names_csv=$names_csv"
+
+    test -n "$force" && return 
+    read -rp "Variables above correct? (y/n) "
+    test "$REPLY" = "y" || exit 1
+}
+
+csv_delimiter=${csv_delimiter:-","}
+folder=${1:-Bilder}
+names_csv=${names_csv:-"~/Dropbox/Mueller-Mombartz/RPM-Bilder/BilderNamen.csv"}
+force=${force:-""}
 
 parse_input "$@"
-prepare_environment
-rename_files
+
+case "$version" in
+    1) 
+        prepare_environment_v1
+        rename_files_v1
+        ;;
+    2) 
+        rename_files_v2
+        ;;
+esac
+
--- file_renamer/test.sh
+++ file_renamer/test.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
 prepare_folder(){
-    rm -f "$test_folder"/*
+    rm -rf "$test_folder"/*
+    rm -f names.csv
     mkdir -p "$test_folder"
 }
 
-create_csv(){
+create_csv_v1(){
     for should_name in $should_names; do
-        echo "$should_name${csv_delimiter}something0${csv_delimiter}something1${csv_delimiter}something2" >> "$test_folder/"names.csv
+        echo "$should_name${csv_delimiter}something0${csv_delimiter}something1${csv_delimiter}something2" >> names.csv
     done
 }
 
-create_images(){
-    for is_name in $is_names; do
+create_csv_v2(){
+    # TODO make dynamic
+    echo "1.png${csv_delimiter}blatt1${csv_delimiter}first${csv_delimiter}description1${csv_delimiter}something1${csv_delimiter}8-4" >> names.csv
+    echo "2.png${csv_delimiter}blatt2${csv_delimiter}second${csv_delimiter}description2${csv_delimiter}something2${csv_delimiter}8-3" >> names.csv
+    echo "3.png & 4.png${csv_delimiter}blatt2${csv_delimiter}third${csv_delimiter}description3${csv_delimiter}something3${csv_delimiter}8-3" >> names.csv
+}
+
+
+create_images_v1(){
+    for is_name in $is_names_v1; do
         touch "$test_folder/$is_name"
     done
 }
 
-test_images(){
+create_images_v2(){
+    for is_name in $is_names_v2; do
+        touch "$test_folder/$is_name"
+    done
+}
+
+test_images_v1(){
     files="$(ls "$test_folder")"
     minimum_one_file_not_found=
 
@@ -32,16 +47,38 @@ test_images(){
     test -z "$minimum_one_file_not_found" && echo "Success"
 }
 
-is_names="1.png 2.png 3.png"
+test_images_v2(){
+    # TODO make dynamic
+    files=("$test_folder"/8-4/first-1.png "$test_folder"/8-3/second-1.png "$test_folder"/8-3/third-1.png "$test_folder"/8-3/third-2.png)
+    for file in "${files[@]}"; do
+        if ! [ -f "$file" ]; then
+            echo "Fail - Could not find file: $file"
+            return
+        fi
+    done
+
+    echo "Success"
+}
+
+is_names_v1="1.png 2.png 3.png"
+is_names_v2="1.png 2.png 3.png 4.png"
+should_filenames="$is_names_v2"
 should_names="first second third"
 should_suffix=".png"
 csv_delimiter=","
 test_folder="testfiles"
 
 prepare_folder
-create_csv
-create_images
+create_csv_v1
+create_images_v1
+names_csv="$PWD"/names.csv ./script.sh testfiles v1
+test_images_v1
 
-./script.sh testfiles
+prepare_folder
+create_csv_v2
+create_images_v2
+names_csv="$PWD"/names.csv ./script.sh testfiles v2
+test_images_v2
 
-test_images
+
+
